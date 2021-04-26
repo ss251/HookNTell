5deafc159c2b843481bce2d8de0d9e35c5573673
ss251 <ss251@uw.edu>
2021-04-26 01:01:30 -0700

    add links to footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,40 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import footer1 from "../../img/footer.png";
+import { Link } from "react-router-dom";
 
 class Footer extends Component {
-    render() {
-        return (
-            <footer>
-                <div className="flex">
-                    <div className="left">
-                        <h6>
-                            Husky Devs
-                        </h6>
-                        <p>
-                            NE Pacific St, University, Washington 98105, United States
-                        </p>
-                    </div>
-                    <div className="middle">
-                        <img src={footer1} />
-                    </div>
-                    <div className="right">
-                        <p>
-                            Home
-                        </p>
-                        <p>
-                            About
-                        </p>
-                        <p>
-                            Map
-                        </p>
-                        <p>
-                            Profile
-                        </p>
-                    </div>
-                </div>
-                
-            </footer>
-        )
-    }
+  render() {
+    return (
+      <footer>
+        <div className="flex">
+          <div className="left">
+            <h6>Husky Devs</h6>
+            <p>NE Pacific St, University, Washington 98105, United States</p>
+          </div>
+          <div className="middle">
+            <img src={footer1} />
+          </div>
+          <div className="right">
+            <p>
+              <Link to="/">Home</Link>
+            </p>
+            <p>
+              <Link to="/about">About</Link>
+            </p>
+            <p>
+              <Link to="/map">Map</Link>
+            </p>
+            <p>
+              <Link to="/dashboard">Profile</Link>
+            </p>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
 export default Footer;
