bf02da1726328c1c7addaa91fe116971e29fe3c1
ss251 <ss251@uw.edu>
2021-04-30 07:24:18 -0700

    conditional form rendering init

--- client/src/components/profile-forms/AddCatch.js
+++ client/src/components/profile-forms/AddCatch.js
@@ -7,6 +7,7 @@ import emailjs from "emailjs-com";
 
 const AddCatch = ({ addCatches, history }) => {
   const [formData, setFormData] = useState({
+    fishtype: "",
     areacode: "",
     species: "",
     date: "",
@@ -18,6 +19,7 @@ const AddCatch = ({ addCatches, history }) => {
   });
 
   const {
+    fishtype,
     areacode,
     species,
     date,
@@ -51,6 +53,109 @@ const AddCatch = ({ addCatches, history }) => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const changeSelectOptionHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const renderSelectedForm = (param) => {
+    switch (param) {
+      case "Salmon":
+        return (
+          <Fragment>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="* Area Code"
+                name="areacode"
+                value={areacode}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="* Species"
+                name="species"
+                value={species}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="date"
+                placeholder="* Date"
+                name="date"
+                value={date}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="* Location"
+                name="location"
+                value={location}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="* Weight (lbs)"
+                name="weight"
+                value={weight}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="* Length (in)"
+                name="length"
+                value={length}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="* Habitat (Clip Type)"
+                name="habitat"
+                value={habitat}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <textarea
+                name="notes"
+                cols="30"
+                rows="5"
+                placeholder="Notes"
+                value={notes}
+                onChange={onChange}
+              />
+            </div>
+          </Fragment>
+        );
+      case "form_name2":
+        return (
+          <form name="form_name1" id="form_name2">
+            form 2
+          </form>
+        );
+      case "form_name3":
+        return (
+          <form name="form_name1" id="form_name3">
+            form 3
+          </form>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <Fragment>
       <h1 className="large text-primary">Report Catch</h1>
@@ -59,86 +164,27 @@ const AddCatch = ({ addCatches, history }) => {
         className="form"
         onSubmit={(e) => {
           e.preventDefault();
-          sendEmail(e);
+          //sendEmail(e);
           addCatches(formData, history);
         }}
       >
         <div className="form-group">
-          <input
-            type="text"
-            placeholder="* Area Code"
-            name="areacode"
-            value={areacode}
-            onChange={onChange}
-            required
-          />
+          <select
+            className="form-group-select"
+            name="fishtype"
+            value={fishtype}
+            onChange={changeSelectOptionHandler}
+          >
+            <option>* Select Catch</option>
+            <option value="Salmon">Salmon</option>
+            <option value="Sturgeon">Sturgeon</option>
+            <option value="Halibut">Halibut</option>
+            <option value="Steelhead">Steelhead</option>
+            <option value="DungenessCrab">Dungeness Crab</option>
+          </select>
         </div>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="* Species"
-            name="species"
-            value={species}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="date"
-            placeholder="* Date"
-            name="date"
-            value={date}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="* Location"
-            name="location"
-            value={location}
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="* Weight (lbs)"
-            name="weight"
-            value={weight}
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="* Length (in)"
-            name="length"
-            value={length}
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="* Habitat (Clip Type)"
-            name="habitat"
-            value={habitat}
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-          <textarea
-            name="notes"
-            cols="30"
-            rows="5"
-            placeholder="Notes"
-            value={notes}
-            onChange={onChange}
-          />
-        </div>
+        {renderSelectedForm(formData.fishtype)}
+
         <input type="submit" className="btn btn-primary my-1" />
         <Link className="btn btn-light my-1" to="/dashboard">
           Go Back
---
+++ client/src/components/profile-forms/Salmon.js
@@ -0,0 +1,77 @@
+{
+  /* <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Area Code"
+            name="areacode"
+            value={areacode}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Species"
+            name="species"
+            value={species}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="date"
+            placeholder="* Date"
+            name="date"
+            value={date}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Location"
+            name="location"
+            value={location}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Weight (lbs)"
+            name="weight"
+            value={weight}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Length (in)"
+            name="length"
+            value={length}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Habitat (Clip Type)"
+            name="habitat"
+            value={habitat}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            name="notes"
+            cols="30"
+            rows="5"
+            placeholder="Notes"
+            value={notes}
+            onChange={onChange}
+          />
+        </div> */
+}
