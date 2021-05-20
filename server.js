ba65f67c0f2ad99ad0f31d217f53e58f60dcb65e
ss251 <ss251@uw.edu>
2021-05-20 14:45:50 -0700

    fix landing font issue

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Connect Database
 connectDB();
 
 // Init Middleware
 app.use(express.json());
-
-app.use(cors());
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
